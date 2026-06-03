ccb8741bc8e201c0f8c14dd4b3e02e798979bb1a
Maysun Alharbi <maysun.alharbii@gmail.com>
2026-06-03 19:19:30 +0300

    updated Cases done

--- src/main/java/com/example/redthreadgame/Controller/CaseController.java
+++ src/main/java/com/example/redthreadgame/Controller/CaseController.java
@@ -23,18 +23,18 @@ public class CaseController {
         return ResponseEntity.ok(caseService.getPublishedCases());
     }
 
-    @PostMapping("add/{adminId}")
+    @PostMapping("/add/{adminId}")
     public ResponseEntity<ApiResponse> addCase(@PathVariable Integer adminId, @RequestBody @Valid CaseIn dto) {
         caseService.addCase(adminId, dto);
         return ResponseEntity.status(201).body(new ApiResponse("Case added successfully"));
     }
 
-    @PutMapping("update/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<?> updateCase(@PathVariable Integer id, @RequestBody @Valid CaseIn dto) {
         caseService.updateCase(id, dto);
         return ResponseEntity.status(200).body(new ApiResponse("Case updated successfully"));
     }
-    @DeleteMapping("delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteCase(@PathVariable Integer id) {
         caseService.deleteCase(id);
         return ResponseEntity.ok(new ApiResponse("Case deleted successfully"));
